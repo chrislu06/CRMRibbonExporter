9924ab65ccdc518e89471ecac88072ca75f91642
shaunpickford <shaun9442>
2012-09-26 18:44:40 -0500

    added initial form...doesn't do anything yet

---
+++ InitialForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Microsoft.Crm.Sdk.Samples
+{
+    public partial class InitialForm : Form
+    {
+        public InitialForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitialForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
